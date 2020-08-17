b0c5d1fec6f247b8f60099699ab85e22ee332291
joyce <joyce.zhou@westpac.com.au>
2020-08-17 22:04:01 +1000

    updated

--- my-developer-portal-app/src/components/ADRList.js
+++ my-developer-portal-app/src/components/ADRList.js
@@ -1,30 +1,32 @@
 import React from "react";
-import { Button, ListGroup } from "reactstrap";
+import { Button, ListGroup, ListGroupItem } from "reactstrap";
 
 // function, input props, no render, no this
 function ADRList(props) {
   return (
-    <ul>
+    <ListGroup>
       {props.getADRResult.map((ADR) => {
         return (
-          <li className="liColour" key={ADR._id}>
+          <ListGroupItem className="liColour" key={ADR._id}>
             User Name: {ADR.username} Legal Entity Name: {ADR.legalEntityName}
             {"     "}
             Industry: {ADR.industry}
-            Data Recipient Products: {ADR.softwareProductName} Data Recipient
-            Product Description: {ADR.softwareProductDescription}
+            {/* Data Recipient Products: {ADR.softwareProductName} Data Recipient
+            Product Description: {ADR.softwareProductDescription} */}
             <Button
-              className="gapInButton"
+              color="danger"
               onClick={() => props.deleteADR(ADR._id)}
               variant="primary"
             >
               Remove
             </Button>
-            <Button onClick={() => props.editADR(ADR._id)}>Edit</Button>
-          </li>
+            <Button color="warning" onClick={() => props.editADR(ADR._id)}>
+              Edit
+            </Button>
+          </ListGroupItem>
         );
       })}
-    </ul>
+    </ListGroup>
   );
 }
 export default ADRList;
